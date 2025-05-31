d97ac9bb030bcffaa2fe73eafd4629dfd1233f5e
TheoCreates <pandazflipperpanda706@gmail.com>
2025-05-31 23:53:13 +1200

    project commit

---
+++ UnrealEngineLearning/Intermediate/Build/Win64/x64/UnrealEngineLearningEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
+#include "C:/Users/Theo/UnrealEngineGameLearning/UnrealEngineLearning/Intermediate/Build/Win64/x64/UnrealEngineLearningEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Cpp20.h"
+#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
---
+++ UnrealEngineLearning/Source/UnrealEngineLearning/UnrealEngineLearning.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealEngineLearning.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealEngineLearning, "UnrealEngineLearning" );
