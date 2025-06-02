9eb41538fd0748514b0e4decd155d91a0056ef5f
TheoCreates <pandazflipperpanda706@gmail.com>
2025-06-02 23:03:16 +1200

    project resructure

--- UnrealEngineLearning/Intermediate/Build/Win64/x64/UnrealEngineLearningEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Users/Theo/UnrealEngineGameLearning/UnrealEngineLearning/Intermediate/Build/Win64/x64/UnrealEngineLearningEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
